dabc15269142f3629081819f4ebade22e5cef9be
Ash Kumar <58642607+Ashkumar7@users.noreply.github.com>
2024-05-10 21:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.end('70339233');
 });
 
 server.listen(PORT, () => {
