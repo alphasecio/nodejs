b24401a960436c96ff3c5aa6a68143e73c990fad
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2024-07-09 11:49:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+const express = require('express');
+const app = express();
 const http = require('http');
-const PORT = 3000;
+const WebSocket = require('ws');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log("connected");
+
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    
+    try {
+      // Parse the JSON string
+      let receivedObject = JSON.parse(message);
+      console.log('Parsed Object:', receivedObject);
+
+      // Modify the object if needed
+      receivedObject.position.z += 0;
+
+      // Convert the object back to JSON
+      let response = JSON.stringify(receivedObject);
+
+      // Broadcast the message to all connected clients
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(response);
+        }
+      });
+    } catch (error) {
+      console.error('Error parsing message:', error);
+      ws.send(JSON.stringify({ error: 'Invalid JSON' }));
+    }
+  });
+
+  ws.send('Hello! Message From Server!!');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+// Serve static files from 'public' directory
+app.use(express.static('unity_build'));
+
+const listener = server.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
