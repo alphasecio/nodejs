18282881c10d14df078112b4cb3a523caf0a6e3b
alphasec <95212748+alphasecio@users.noreply.github.com>
2024-07-22 17:41:36 +0800

    Update and rename index.js to routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// Serve the index.html file for the root route
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+module.exports = router;
